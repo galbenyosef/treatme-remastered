b3d1c470dc695dbb5dccafa9d43f9978f5e0cdbc
Gal Ben yosef <galbenyosef@gmail.com>
2019-11-08 15:31:45 +0200

    ma

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -5,8 +5,8 @@ import { Provider as StoreProvider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { theme } from './styles/muiTheme'
 
-import './index.css';
-import App from './components/App/App';
+/* import './index.css';
+ */import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import { getStore } from './config/store';
 import { MyCustomProvdier } from './components/MyCustomProvider/MyCustomProvider';
@@ -25,4 +25,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
