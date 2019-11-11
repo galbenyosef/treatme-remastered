5e211d15e81ea4fd67877683b1af7eae8d951db5
Gal Ben yosef <galbenyosef@gmail.com>
2019-11-11 18:04:17 +0200

    test

--- client/src/config/urls.js
+++ client/src/config/urls.js
@@ -2,7 +2,7 @@ export default {
   "connectionString": "mongodb://localhost:27017/yoram",
   "devServer": "http://localhost:5000",
   "devClient": "http://localhost:8080",  
-  "server": "http://www.treatme.site:5000",
-  "client": "http://www.treatme.site",
+  "server": "31.133.100.160",
+  "client": "31.133.100.160",
   "secret": "gal"
 }
